14a6f60468adbee1c8deced7338c036b1ad16ae3
CoffeeCatRailway <dragonlee1002@gmail.com>
2026-04-17 19:46:50 +1000

    Insert physicals in tree node if pos is contained not overlapping

--- src/simulation/solver.rs
+++ src/simulation/solver.rs
@@ -132,7 +132,7 @@ impl Solver {
 
 			edgesX: Vec::new(),
 			edgesY: Vec::new(),
-			quadTree: BSPGrid::new(10, AABB::centered(Vec3::ZERO, worldSize.truncate().extend(0.0))), // todo: fix vec3 issue with aabb/quadtree
+			quadTree: BSPGrid::new(4, AABB::centered(Vec3::ZERO, worldSize.truncate().extend(0.0))), // todo: fix vec3 issue with aabb/quadtree
 			physicals: HashMap::new(),
 			
 			subSteps: 8,
@@ -440,12 +440,13 @@ impl Solver {
 		let now = Instant::now();
 		
 		if self.flags.get(F_COLLISION_MODE) {
-			// 15+ fps
+			// 17+ fps
 			// ~3.5ms
 			self.quadTree.clear();
 			for (_, physical) in self.physicals.iter() {
 				self.quadTree.insert(physical.clone(), &|physical, bounds| {
-					bounds.overlaps(&physical.borrow().bounds())
+					// bounds.overlaps(&physical.borrow().bounds())
+					bounds.containsPoint(physical.borrow().transform().position)
 				});
 			}
 			
